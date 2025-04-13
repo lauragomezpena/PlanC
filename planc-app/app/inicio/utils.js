1e52d0d53e3845d4871bfa7994e5f961d117736c
fernanda marcos <202202335@alu.comillas.edu>
2025-04-13 13:01:51 +0200

    Minor changes

--- planc-app/app/inicio/utils.js
+++ planc-app/app/inicio/utils.js
@@ -1,15 +1,15 @@
 export const doLogin = async (username, password) => {
-    const response = await fetch("https://das-p2-backend.onrender.com/api/users/login/",
-        {
+    const response = await fetch("http://127.0.0.1:8000/api/token/", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ username, password }),
+    });
 
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({ username, password }),
-
-        });
-        
-        const data = await response.json();
-        return data;
-        };
+    const data = await response.json();
+    if (!response.ok) {
+        throw new Error(data.detail || "Error al iniciar sesión");
+    }
+    return data; // Devuelve los tokens
+};
